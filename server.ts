cf4063a3cb7e9fea956368e34f9f24555a294df5
Glen Johnson <plasticarm@gmail.com>
2026-03-09 13:40:22 -0400

    game server connection update

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
 import Pusher from "pusher";
-import { generateRoomCode } from "./utils/roomUtils.ts";
+import { generateRoomCode } from "./utils/roomUtils.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
